4a77ad1ac4a84c43f653de26c337dc9c415ba071
SheriffMudasir <sheriffmudasir2021@gmail.com>
2023-11-26 03:37:12 +0100

    Solved the last mandatpory task

---
+++ 3-quick_sort.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * quick_sort - Sorts an array of integers in ascending order using Quick Sort
+ * @array: The array to be sorted
+ * @size: The size of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+    if (array == NULL || size < 2)
+        return;
+
+    quick_sort_recursive(array, 0, size - 1, size);
+}
+/**
+ * quick_sort_recursive - Recursive helper function for Quick Sort
+ * @array: The array to be sorted
+ * @low: The low index of the partition
+ * @high: The high index of the partition
+ * @size: The size of the array
+ */
+void quick_sort_recursive(int *array, int low, int high, size_t size)
+{
+    int pivot;
+
+    if (low < high)
+    {
+        pivot = partition(array, low, high, size);
+        quick_sort_recursive(array, low, pivot - 1, size);
+        quick_sort_recursive(array, pivot + 1, high, size);
+    }
+}
