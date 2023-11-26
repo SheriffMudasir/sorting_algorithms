7e43a251e33c7d10c4e4c469960aabee9ecb57ea
SheriffMudasir <sheriffmudasir2021@gmail.com>
2023-11-25 18:54:58 -0800

    Quick Sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
 #include "sort.h"
+
+void quick_sort_recursive(int *array, int low, int high, size_t size);
 
 /**
  * quick_sort - Sorts an array of integers in ascending order using Quick Sort
@@ -13,6 +14,7 @@ void quick_sort(int *array, size_t size)
 
     quick_sort_recursive(array, 0, size - 1, size);
 }
+
 /**
  * quick_sort_recursive - Recursive helper function for Quick Sort
  * @array: The array to be sorted
